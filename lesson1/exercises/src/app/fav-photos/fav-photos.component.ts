5506d10842fd0489b1c456c5b7c4d96eb994df0a
Ankayarkanni <ankay30@gmail.com>
2021-04-11 19:45:12 -0500

    lesson1 exercises completed

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Goal Achievement';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_su1A912bTHtERCrdjTg0o5Hh4QSM4xsAxQ&usqp=CAU';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS56zGsTj5S0dHdPFQNg4yf84KuBVtak1Soow&usqp=CAU';
 
   constructor() { }
 
